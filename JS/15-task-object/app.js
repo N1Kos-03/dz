0198883298b8ea00d65acd70523d011849f51606
Никита <kosmynin003@gmail.com>
2025-03-11 14:10:13 +0300

    Изменения

--- JS/15-task-object/app.js
+++ JS/15-task-object/app.js
@@ -1,35 +1,44 @@
-let tasks = [];
-let lastId = 0;
+const ToDoList = {
+  tasks: [],
+  lastId: 0,
 
+  addTask(title, priority) {
+    this.tasks.push({ title, id: ++this.lastId, priority });
+    console.log(`Добавлена задача: ${title}`);
+  },
 
-function addTask(title, priority) {
-  tasks.push({ title, id: ++lastId, priority });
-  console.log(`Добавлена задача: ${title}`);
-}
+  removeTask(id) {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+    console.log(`Удалена задача с id ${id}`);
+  },
 
-function removeTask(id) {
-  tasks = tasks.filter(task => task.id !== id);
-  console.log(`Удалена задача с id ${id}`);
-}
+  updateTask(id, updates) {
+    const task = this.tasks.find(task => task.id === id);
+    if (task) {
+      Object.assign(task, updates);
+      console.log(`Обновлена задача с id ${id}`);
+    } else {
+      console.log(`Задача с id ${id} не найдена`);
+    }
+  },
 
-function updateTask(id, updates) {
-  let task = tasks.find(task => task.id === id);
-  if (task) Object.assign(task, updates);
-  console.log(`Обновлена задача с id ${id}`);
-}
+  sortTasks() {
+    this.tasks.sort((a, b) => a.priority - b.priority);
+    console.log('Задачи отсортированы по приоритету от меньшего к большему');
+  },
 
-function sortTasks() {
-  tasks.sort((a, b) => a.priority - b.priority);
-}
+  showTasks() {
+    console.log('Все задачи:', this.tasks);
+  }
+};
 
+// Пример использования:
+ToDoList.addTask('Помыть посуду', 2);
+ToDoList.addTask('Убрать комнату', 3);
+ToDoList.addTask('Погулять с собакой', 1);
 
-addTask('Помыть посуду', 2);
-addTask('Убрать комнату', 3);
-addTask('Погулять с собакой', 1);
-removeTask(2);
-updateTask(1, { priority: 3});
+ToDoList.removeTask(2);
+ToDoList.updateTask(3, { title: 'Погулять с собаками' });
 
-
-console.log('Все задачи:', tasks);
-console.log('Задачи отсортированы по приоритету от меньшего к большему:', tasks),sortTasks('priority');
-
+ToDoList.sortTasks();
+ToDoList.showTasks();
