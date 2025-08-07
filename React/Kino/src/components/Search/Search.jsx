31eac185c95492c5ecf72a380e239c000bf5b8d7
Никита <kosmynin003@gmail.com>
2025-08-07 14:14:20 +0300

    ДЗ

--- React/Kino/src/components/Search/Search.jsx
+++ React/Kino/src/components/Search/Search.jsx
@@ -1,35 +1,39 @@
 import React, { useState } from 'react';
 import Input from '../Input/Input';
 import Button from '../Button/Button';
-import './Search.css'
+import styles from './Search.module.css';
+import indexstyles from '../../index.module.css';
 
-function Search({onSearch}) { 
- const [inputData, setInputData] = useState('');
+function Search({ onSearch }) {
+  const [inputData, setInputData] = useState('');
 
-const inputChange = (event) => {
-  setInputData(event.target.value);
-};
-
+  const inputChange = (event) => {
+    setInputData(event.target.value);
+  };
 
   const handleSearch = () => {
     if (onSearch) onSearch(inputData);
-    console.log('Отправлено:', inputData);
+    
   };
 
 
 
   return (
-    <div className='search-body'>
-    <h1>Поиск</h1>
-    <p className='p-16'>Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.</p>
-    <div className='search'>
-      <Input leftIcon={<img src="./public/search.svg" alt="Поиск" />} 
-      type="text" 
-      value={inputData} 
-      onChange={inputChange}  
-      placeholder="Введите название"/>
-      <Button onClick={handleSearch} text="Искать"/>
-    </div>
+    <div className={styles['search-body']}>
+      <h1>Поиск</h1>
+      <p className={indexstyles['p-16']}>
+        Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.
+      </p>
+      <div className={styles['search']}>
+        <Input
+          leftIcon={<img src="./public/search.svg" alt="Поиск" />}
+          type="text"
+          value={inputData}
+          onChange={inputChange}
+          placeholder="Введите название"
+        />
+        <Button onClick={handleSearch} text="Искать" />
+      </div>
     </div>
   );
 }
