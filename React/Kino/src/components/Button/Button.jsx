7797316521c644f64b3b9776b1b523715dc716bb
Никита <kosmynin003@gmail.com>
2025-08-18 12:58:50 +0300

    test

--- React/Kino/src/components/Button/Button.jsx
+++ React/Kino/src/components/Button/Button.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 
 const Button = React.forwardRef(({text, onClick}, ref) => {
-  return <button ref={ref} className={styles['button']} onClick={onClick}>{text}</button>
+  return <button ref={ref} className={styles['button']} onClick={onClickk}>{text}</button>
 })
 
 export default Button
